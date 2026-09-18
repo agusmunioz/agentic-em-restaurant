79dbc20b9f5818fb64ca137ca7c286ef34d2e511
agusmunioz <agustinmunioz@gmail.com>
2026-09-18 08:30:15 -0300

    start all over

--- dist/src/util/assertions.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.assertNotEmpty = assertNotEmpty;
-function assertNotEmpty(value) {
-    if (value === null || value === undefined) {
-        throw new Error("Expected non-empty value");
-    }
-    return value;
-}
--- migrations/V2__tables.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "public"."tables"
-(
-    table_id     TEXT PRIMARY KEY,
-    table_number INTEGER NOT NULL,
-    seats        INTEGER NOT NULL
-);
